3344f1ab8dcd83f6ce7ab386f23c720ee3ddd7bc
Matt Trent <matt_trent@hotmail.com>
2023-06-09 11:10:57 +0100

    Added Constants File

---
+++ Java/src/main/java/com/gildedrose/Constants.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+public final class Constants {
+
+    private Constants() {
+            // restrict instantiation
+    }
+
+    public static final String sulfurasName = "Sulfuras, Hand of Ragnaros";
+    public static final String agedBrie = "Aged Brie";
+    public static final String backStagePass = "Backstage passes to a TAFKAL80ETC concert";
+}
